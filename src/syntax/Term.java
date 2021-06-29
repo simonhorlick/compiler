317074ecce29d4114562173289ca6b9b21990ae3
Simon Horlick <simonhorlick@gmail.com>
2021-06-29 21:31:12 +0800

    Comments

--- src/syntax/Term.java
+++ src/syntax/Term.java
@@ -2,10 +2,10 @@ package syntax;
 
 // Term describes things that can be evaluated.
 interface Term {
-  //
+  // Returns true if the term is a value (either zero, or successors of zero).
   boolean isNumericVal();
 
-  //
+  // Returns true if the term is a value (numeric or boolean).
   boolean isVal();
 
   // Evaluate this term by a single step.
