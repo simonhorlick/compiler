eab9a82ec1272b477632ec96031b4e453904b8b6
Simon Horlick <simonhorlick@gmail.com>
2021-06-30 22:57:40 +0800

    parser: Remove unused files

--- src/parser/AbsToken.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-public class AbsToken {
-  private final String name;
-
-  public AbsToken(String name) {
-    this.name = name;
-  }
-}
--- src/parser/ParseError.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-public class ParseError {
-  private final String reason;
-
-  public ParseError(String reason) {
-    this.reason = reason;
-  }
-}
